79ffb2f334ff9a3e76d49c55b2ea03c3f240fedd
yycg <yingyingchongge@gmail.com>
2020-10-14 15:43:35 +0800

    app index

--- src/components/home/AppIndex.vue
+++ src/components/home/AppIndex.vue
@@ -1,24 +1,32 @@
 <template>
-  <el-container>
-    <el-aside style="width: 200px; margin-top: 20px">
-      <switch></switch>
-      <side-menu></side-menu>
-    </el-aside>
-    <el-main>
-      <div style="padding: 24px 48px">
-        <h1>:)</h1>
-        <p>
-          LiveMusic 个性化现场音乐活动推荐<br />
-          <span style="font-size: 30px"></span>
-        </p>
-        <span style="font-size: 22px">
-          [<a href="release/LiveMusic-1.0.0.apk"
-            >点此下载1.0.0版本安装包（仅限Android系统）</a
-          >]
-        </span>
-      </div>
-    </el-main>
-  </el-container>
+  <a-layout id="components-layout-demo-top-side-2">
+    <a-layout>
+      <a-layout-sider width="200" style="background: #fff">
+        <side-menu :sideMenuList="sideMenuList" :selectedKeys="selectedKeys"></side-menu>
+      </a-layout-sider>
+      <a-layout style="padding: 0 24px 24px">
+        <a-breadcrumb style="margin: 16px 0">
+          <a-breadcrumb-item>首页</a-breadcrumb-item>
+        </a-breadcrumb>
+
+        <a-card id="introduction" title="介绍" :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
+          介绍
+        </a-card>
+        <br>
+
+        <a-card id="technology" title="技术" :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
+          技术
+        </a-card>
+        <br>
+
+        <a-card id="algorithm" title="算法" :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
+          算法
+        </a-card>
+        <br>
+
+      </a-layout>
+    </a-layout>
+  </a-layout>
 </template>
 
 <script>
@@ -27,44 +35,16 @@ export default {
   components: { SideMenu },
   data () {
     return {
-      sideBarList: [
-        { href: '#', name: '介绍' },
-        { href: '#', name: '技术' }
-      ]
+      sideMenuList: [
+        { href: '#introduction', name: '介绍', type: 'user' },
+        { href: '#technology', name: '技术', type: 'laptop' },
+        { href: '#algorithm', name: '算法', type: 'notification' }
+      ],
+      selectedKeys: []
     }
   }
 }
 </script>
 
 <style scoped>
-* {
-  padding: 0;
-  margin: 0;
-}
-.think_default_text {
-  padding: 4px 48px;
-}
-a {
-  color: #2e5cd5;
-  cursor: pointer;
-  text-decoration: none;
-}
-a:hover {
-  text-decoration: underline;
-}
-body {
-  background: #fff;
-  font-family: "Century Gothic", "Microsoft yahei";
-  color: #333;
-  font-size: 18px;
-}
-h1 {
-  font-size: 100px;
-  font-weight: normal;
-  margin-bottom: 12px;
-}
-p {
-  line-height: 1.6em;
-  font-size: 42px;
-}
 </style>
