bd20928592e53c32bb07b4ed2a8514fb50257e6c
yycg <yingyingchongge@gmail.com>
2020-10-14 10:38:24 +0800

    Avoid mutating a prop directly since the value will be overwritten whenever the parent component re-renders. Instead, use a data or computed property based on the prop's value. Prop being mutated: "start"

--- src/components/common/Pagination.vue
+++ src/components/common/Pagination.vue
@@ -4,8 +4,8 @@
       show-quick-jumper
       show-size-changer
       :current="current"
-      :total="total"
-      :pageSize="count"
+      :total="totalProp"
+      :pageSize="countProp"
       @showSizeChange="onShowSizeChange"
       @change="onChange"
     />
@@ -15,13 +15,21 @@
 <script>
 export default {
   props: ['count', 'start', 'total', 'categoryList'],
+  data () {
+    return {
+      countProp: this.count,
+      startProp: this.start,
+      totalProp: this.total,
+      categoryListProp: this.categoryList
+    }
+  },
   computed: {
     current: {
       get: function () {
-        return this.start / this.count + 1
+        return this.startProp / this.countProp + 1
       },
       set: function (newValue) {
-        this.start = (newValue - 1) * this.count;
+        this.startProp = (newValue - 1) * this.countProp;
       }
     }
   },
@@ -36,31 +44,30 @@ export default {
   methods: {
     onShowSizeChange(current, pageSize) {
       console.log(current, pageSize)
-      this.count = pageSize
+      this.countProp = pageSize
       this.current = 1
-      console.log(this.start, this.count, this.total)
       this.queryEventList()
     },
     onChange(pageNumber) {
       console.log('Page: ', pageNumber)
       this.current = pageNumber
-      console.log(this.start, this.count, this.total)
       this.queryEventList()
     },
     queryEventList() {
-      if (this.categoryList.length == 0) {
+      console.log(this.categoryListProp)
+      if (this.categoryListProp.length == 0) {
         this.$emit('selectCategory')
       } else {
         this.$axios.get('/event/category', {
           params: {
-            category: this.categoryList[0],
-            subcategory: this.categoryList.length > 1 ? this.categoryList[1] : null,
-            start: this.start,
-            count: this.count
+            category: this.categoryListProp[0],
+            subcategory: this.categoryListProp.length > 1 ? this.categoryListProp[1] : null,
+            start: this.startProp,
+            count: this.countProp
           }
         }).then(res => {
           console.log(res.data)
-          this.$emit('selectCategory', res.data.data, this.categoryList)
+          this.$emit('selectCategory', res.data.data, this.categoryListProp)
         })
       }
     }
