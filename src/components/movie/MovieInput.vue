2863df72c85a0393e89c627d85c39afee61bff64
yycg <yingyingchongge@gmail.com>
2020-10-20 21:06:25 +0800

    movie input

--- src/components/movie/MovieInput.vue
+++ src/components/movie/MovieInput.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-input-search style="margin-bottom: 8px" placeholder="请输入电影关键词" @change="onSearch" />
+  <a-input-search style="margin-bottom: 8px" placeholder="请输入电影关键词" @search="onSearch" />
 </template>
 
 <script>
@@ -8,17 +8,15 @@ export default {
     onSearch (value) {
       console.log('onSearch', value)
       this.$emit('load')
-      this.loading = true
-      this.keyword = value.data
       this.$axios.get('/movie/search', {
         params: {
-          keyword: value.data,
+          keyword: value,
           start: 0,
           count: 10
         }
       }).then(res => {
         console.log(res.data)
-        this.$emit('search', res.data.data, value.data)
+        this.$emit('search', res.data.data, value)
       })
     }
   }
